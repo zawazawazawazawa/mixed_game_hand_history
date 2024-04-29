1d5d0916bc350a6a93e3244a5913391a8ecee0dd
zawazawazawazawa <bosse.de.poulet@gmail.com>
2024-04-29 18:32:13 +0900

    :recycle: Startページを別ファイルに切り出す

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 
 import 'my_app_state.dart';
+import 'start_page.dart';
 
 void main() {
   runApp(
@@ -70,36 +71,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             );
           }
-        );
-      }
-    );
-  }
-}
-
-class StartPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<MyAppState>(
-      builder: (context, state, child) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 10),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // BigBlindページへ遷移
-                      state.updateSelectedIndex('blind');
-                    },
-                    child: Text('Start'),
-                  ),
-                ],
-              ),
-            ],
-          ),
         );
       }
     );
---
+++ lib/start_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'my_app_state.dart';
+
+class StartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MyAppState>(
+      builder: (context, state, child) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // BigBlindページへ遷移
+                      state.updateSelectedIndex('blind');
+                    },
+                    child: Text('Start'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
