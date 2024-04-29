fd29663e9a2639246c167ccc75774db226ec7e63
zawazawazawazawa <bosse.de.poulet@gmail.com>
2024-04-29 22:05:11 +0900

    :beer: postion名のlabelをつける

--- lib/preflop_page.dart
+++ lib/preflop_page.dart
@@ -7,35 +7,54 @@ class PreflopPage extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
     return Consumer<MyAppState>(
-      builder: (context, state, child) => Column(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(state.participants, (index) {
-                return RadioAction();
+      builder: (context, state, child) {
+        List<String> positions = ['BB', 'SB', 'BTN', 'CO', 'HJ', 'LJ', 'UTG'];
+
+        if (state.participants == 8) {
+          positions.insert(6, 'UTG+1');
+        } else if (state.participants == 9) {
+          positions.insertAll(6, ['UTG+2', 'UTG+1']);
+        }
+
+        return Column(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(state.participants, (index) {
+                  return RadioAction(positionName: positions[index]);
+                },
+              ).reversed.toList()
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // blindページへ遷移
+                state.updateSelectedIndex('participants');
               },
-            )
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // blindページへ遷移
-              state.updateSelectedIndex('participants');
-            },
-            child: Text('Blindに戻る'),
-          ),
-        ]
-      )
+              child: Text('Blindに戻る'),
+            ),
+          ]
+        );
+      }
     );
   }
 }
 
 class RadioAction extends StatefulWidget {
+  final String positionName;
+
+  RadioAction({required this.positionName});
+
   @override
   _RadioActionState createState() => _RadioActionState();
 }
 
 class _RadioActionState extends State<RadioAction> {
   String? _selectedAction = 'fold';
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   void _handleRadioValueChange(String? value) {
     setState(() {
@@ -45,42 +64,47 @@ class _RadioActionState extends State<RadioAction> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Expanded(
-          child: ListTile(
-            title: Text('Raise'),
-            leading: Radio<String>(
-              value: 'raise',
-              groupValue: _selectedAction,
-              onChanged: _handleRadioValueChange,
-            ),
-          ),
-        ),
-        Expanded(
-          child:
-            ListTile(
-              title: Text('Call'),
-              leading: Radio<String>(
-                value: 'call',
-                groupValue: _selectedAction,
-                onChanged: _handleRadioValueChange,
+    return Column(
+      children: [
+        Text(widget.positionName),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: ListTile(
+                title: Text('Raise'),
+                leading: Radio<String>(
+                  value: 'raise',
+                  groupValue: _selectedAction,
+                  onChanged: _handleRadioValueChange,
+                ),
               ),
             ),
-        ),
-        Expanded(
-          child: 
-            ListTile(
-              title: Text('Fold'),
-              leading: Radio<String>(
-                value: 'fold',
-                groupValue: _selectedAction,
-                onChanged: _handleRadioValueChange,
-              ),
+            Expanded(
+              child:
+                ListTile(
+                  title: Text('Call'),
+                  leading: Radio<String>(
+                    value: 'call',
+                    groupValue: _selectedAction,
+                    onChanged: _handleRadioValueChange,
+                  ),
+                ),
             ),
-        )
-      ] 
+            Expanded(
+              child: 
+                ListTile(
+                  title: Text('Fold'),
+                  leading: Radio<String>(
+                    value: 'fold',
+                    groupValue: _selectedAction,
+                    onChanged: _handleRadioValueChange,
+                  ),
+                ),
+            )
+          ] 
+        ),
+      ],
     );
   }
 }
