f894997b09afe803635b215d4fc40673ba9d3dcc
zawazawazawazawa <bosse.de.poulet@gmail.com>
2024-05-02 12:17:57 +0900

    ポジションごとのactionをバラした

--- lib/preflop_page.dart
+++ lib/preflop_page.dart
@@ -3,6 +3,14 @@ import 'package:provider/provider.dart';
 
 import 'my_app_state.dart';
 
+class UnexpectedPositionError extends Error {
+  final String message;
+  UnexpectedPositionError(this.message);
+
+  @override
+  String toString() => message;
+}
+
 class PreflopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -48,20 +56,95 @@ class RadioAction extends StatefulWidget {
 }
 
 class _RadioActionState extends State<RadioAction> {
-  String? _selectedAction = null;
+  late List<String> _positions;
+
+  String? _utgSelectedAction = null;
+  String? _utg1SelectedAction = null;
+  String? _utg2SelectedAction = null;
+  String? _ljSelectedAction = null;
+  String? _hjSelectedAction = null;
+  String? _coSelectedAction = null;
+  String? _btnSelectedAction = null;
+  String? _sbSelectedAction = null;
+  String? _bbSelectedAction = null;
+
   int _lastHandled = 0;
 
   @override
   void initState() {
     super.initState();
+    _positions = widget.positions;
   }
 
-  // TODO
-  // Actionが全部一つのstateになっているので分離しないといけない
+  String? getActionStateFromIndex(int index) {
+    switch (_positions[_positions.length - index - 1]) {
+      case 'BB':
+        return _bbSelectedAction;
+      case 'SB':
+        return _sbSelectedAction;
+      case 'BTN':
+        return _btnSelectedAction;
+      case 'CO':
+        return _coSelectedAction;
+      case 'HJ':
+        return _hjSelectedAction;
+      case 'LJ':
+        return _ljSelectedAction;
+      case 'UTG+2':
+        return _utg2SelectedAction;
+      case 'UTG+1':
+        return _utg1SelectedAction;
+      case 'UTG':
+        return _utgSelectedAction;
+      default:
+        throw UnexpectedPositionError(
+            'unexpected position name is detected: ${_positions[_positions.length - _lastHandled - 1]}');
+    }
+  }
+
   void _handleRadioValueChange(String? value) {
     setState(() {
-      _selectedAction = value;
-      _lastHandled = _lastHandled + 1;
+      switch (_positions[_positions.length - _lastHandled - 1]) {
+        case 'BB':
+          _bbSelectedAction = value;
+
+          break;
+        case 'SB':
+          _sbSelectedAction = value;
+
+          break;
+        case 'BTN':
+          _btnSelectedAction = value;
+
+          break;
+        case 'CO':
+          _coSelectedAction = value;
+
+          break;
+        case 'HJ':
+          _hjSelectedAction = value;
+
+          break;
+        case 'LJ':
+          _ljSelectedAction = value;
+
+          break;
+        case 'UTG+2':
+          _utg2SelectedAction = value;
+
+          break;
+        case 'UTG+1':
+          _utg1SelectedAction = value;
+
+          break;
+        case 'UTG':
+          _utgSelectedAction = value;
+
+          break;
+        default:
+          throw UnexpectedPositionError(
+              'unexpected position name is detected: ${_positions[_positions.length - _lastHandled - 1]}');
+      }
     });
   }
 
@@ -70,7 +153,6 @@ class _RadioActionState extends State<RadioAction> {
     return Column(
         children: List<Widget>.generate(widget.positions.length, (index) {
       return Visibility(
-        //mainAxisAlignment: MainAxisAlignment.center,
         visible: index <= _lastHandled,
         child: Row(
           children: [
@@ -80,7 +162,7 @@ class _RadioActionState extends State<RadioAction> {
                 title: Text('Raise'),
                 leading: Radio<String>(
                   value: 'raise',
-                  groupValue: _selectedAction,
+                  groupValue: getActionStateFromIndex(index),
                   onChanged: _handleRadioValueChange,
                 ),
               ),
@@ -90,7 +172,7 @@ class _RadioActionState extends State<RadioAction> {
                 title: Text('Call'),
                 leading: Radio<String>(
                   value: 'call',
-                  groupValue: _selectedAction,
+                  groupValue: getActionStateFromIndex(index),
                   onChanged: _handleRadioValueChange,
                 ),
               ),
@@ -100,7 +182,7 @@ class _RadioActionState extends State<RadioAction> {
                 title: Text('Fold'),
                 leading: Radio<String>(
                   value: 'fold',
-                  groupValue: _selectedAction,
+                  groupValue: getActionStateFromIndex(),
                   onChanged: _handleRadioValueChange,
                 ),
               ),
